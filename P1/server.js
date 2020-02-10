87bd4949b20e51a3b8fd28483b266e6557dcf1a3
daguadod <dazma97@gmail.com>
2020-02-10 11:40:15 +0100

    Añadido módulo URL

--- P1/server.js
+++ P1/server.js
@@ -1,9 +1,9 @@
+//-- Modulo https
+const http = require('http');
 //-- Puerto donde recibir las peticiones
 const PUERTO = 8080;
-
-//-- Modulo http
-const http = require('http');
-
+// --Modulo URL
+const url = require('url');
 //-- Acceso al módulo fs, para lectura de ficheros
 const fs = require('fs');
 
@@ -15,10 +15,21 @@ console.log("Arrancando servidor...")
 function peticion(req, res) {
 
   //-- Peticion recibida
-  console.log(req.url)
+  console.log("Recurso solucitado (URL): " + req.url)
 
+  let q = url.parse(req.url, true);
   //-- Crear mensaje de respuesta
+  console.log("Pathname: " +  q.pathname)
+  console.log("search: " + q.search)
+  console.log("Búsqueda:")
+  let qdata = q.query
+  console.log(qdata)
 
+  //-- Acceso al objeto
+  console.log("Artículo: " + qdata.articulo)
+  console.log("Color: " + qdata.color)
+
+  console.log()
   fs.readFile('index.html', 'utf8', (err, data) => {
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
