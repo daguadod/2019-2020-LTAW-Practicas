5872f6db055bd7c378cacd3c4349163f172d2422
daguadod <dazma97@gmail.com>
2020-02-10 11:58:13 +0100

    Añadiendo fallos y provocandolos "404 Not Found"

--- P1/server.js
+++ P1/server.js
@@ -12,13 +12,17 @@ console.log("Arrancando servidor...")
 //-- Funcion para atender a una Peticion
 //-- req: Mensaje de solicitud
 //-- res: Mensaje de respuesta
-function peticion(req, res) {
+function petition(req, res) {
 
   //-- Peticion recibida
+  console.log("Peticion recibida");
   console.log("Recurso solucitado (URL): " + req.url)
 
   let q = url.parse(req.url, true);
+
   //-- Crear mensaje de respuesta
+
+  //Parte de query
   console.log("Pathname: " +  q.pathname)
   console.log("search: " + q.search)
   console.log("Búsqueda:")
@@ -29,11 +33,27 @@ function peticion(req, res) {
   console.log("Artículo: " + qdata.articulo)
   console.log("Color: " + qdata.color)
 
+  //_- Crear el mensaje de respuesta. Primero la cabecera
+  //-- El código 200 se usa para indicar que todo está ok
+  //-- En el campo Content-Type tenemos que introducir el tipo MIME
+  //-- de lo que devolvemos
+  let mime = "text/html"
+  res.writeHead(200, {'Content-Type': mime});
+
+  //Leer fihchero html
   console.log()
-  fs.readFile('index.html', 'utf8', (err, data) => {
+  fs.readFile('index1.html', 'utf8', (err, data) => {
+
+    //--Fichero no encontrado
+    if (err) {
+      res.writeHead(404, {'Content-Type': 'text/html'});
+      return res.end("404 Not Found");
+    }
+
+    //-- Tipo mime por defecto html
     res.writeHead(200, {'Content-Type': 'text/html'});
     res.write(data);
-    return res.end();
+    res.end();
   });
 
 }
@@ -42,7 +62,7 @@ function peticion(req, res) {
 //-- Inicializar el servidor
 //-- Cada vez que recibe una petición
 //-- invoca a la funcion peticion para atenderla
-const server = http.createServer(peticion)
+const server = http.createServer(petition)
 
 //-- Configurar el servidor para escuchar en el
 //-- puerto establecido
