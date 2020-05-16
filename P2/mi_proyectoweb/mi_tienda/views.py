49b9b97620d053ccc4e18c37f32b8f92928e76f7
daguadod <dazma97@gmail.com>
2020-05-16 20:06:54 +0200

    Terminada primera parte de prueba de django

--- P2/mi_proyectoweb/mi_tienda/views.py
+++ P2/mi_proyectoweb/mi_tienda/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.template import Template, Context
+from django.template.loader import get_template
 from django.shortcuts import render
 from random import randint
 
@@ -33,3 +35,64 @@ def test1(request):
     </html>
     """
     return HttpResponse(PAGINA_INI + P + PAGINA_FIN)
+
+    # -- Ejemplo de generacion mediante una plantilla en el código
+def test2(request):
+
+    # -- Obtener el número aleatorio
+    numero = randint(0, 100)
+
+    PLANTILLA = """
+    <!DOCTYPE html>
+    <html lang="es" dir="ltr">
+      <head>
+        <meta charset="utf-8">
+        <title>Test2</title>
+      </head>
+      <body>
+        <h1>TEST2</h1>
+        <p> Numero aleatorio:  {{numero}} </p>
+      </body>
+    </html>
+    """
+
+    # --Procesar la plantilla
+    t = Template(PLANTILLA)
+
+    # -- Crear el contexto: Asignar el numero
+    c = Context({'numero': str(numero)})
+
+    # -- Obtener la pagina html final
+    html = t.render(c)
+
+    return HttpResponse(html)
+
+# -- Ejemplo de generacion mediante una plantilla en FICHERO
+def test3(request):
+
+    # -- Obtener el número aleatorio
+    numero = randint(0, 100)
+
+    # -- Leer la plantilla del fichero
+    t = get_template('test.html')
+
+    # -- Crear el contexto: Asignar el numero
+    c = {'numero': str(numero)}
+
+    # -- Obtener la pagina html final
+    html = t.render(c)
+
+    return HttpResponse(html)
+
+    # -- Ejemplo de uso de la función Render
+def test4(request):
+    # -- Obtener el número aleatorio
+    numero = randint(0, 100)
+    return render(request, 'test.html', {'numero':str(numero)})
+
+
+
+def test5(request):
+    # -- Obtener el número aleatorio
+    numero = randint(0, 100)
+    return render(request, 'test5.html', {'numero':str(numero)})
